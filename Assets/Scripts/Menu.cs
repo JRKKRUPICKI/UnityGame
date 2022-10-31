7f44f59a9645aa45f9e2db2d8d025d8f7b38f3db
JRKKRUPICKI <jrkkrupicki@gmail.com>
2022-10-31 20:01:46 +0100

    Zmiana organizacji plików

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10, 10, 100, 50), "Graj"))
+        {
+            SceneManager.LoadScene(1);
+            return;
+        }
+        if (GUI.Button(new Rect(10, 70, 100, 50), "Wyjdü"))
+        {
+            Application.Quit();
+        }
+    }
+}
